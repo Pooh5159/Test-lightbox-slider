5a3f25c5d87d6e9e44a535e53083563e6aa9e8da
Pooh5159 <92722233+Pooh5159@users.noreply.github.com>
2022-01-18 23:19:39 +0100

    Add files via upload

--- scriptslider.js
+++ scriptslider.js
@@ -1,12 +1,19 @@
 var slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
+// showSlides(slideIndex);
+
+function plusSlides(id,n) {
+  let test = document.getElementById(id);
+  test.childNodes[1].style.opacity = "0.5";
   showSlides(slideIndex += n);
+  console.log(test, test.childNodes);
 }
-function currentSlide(n) {
+
+function currentSlide(id,n) {
+  document.getElementById(id);
   showSlides(slideIndex = n);
 }
-function showSlides(n) {
+
+function showSlides(n) {  
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
