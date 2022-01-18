37b8028d06ef79cf792f894a5a875e218d64e4e0
Pooh5159 <92722233+Pooh5159@users.noreply.github.com>
2022-01-18 18:33:50 +0100

    Add files via upload

---
+++ scriptlightbox.js
@@ -0,0 +1,22 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("#myImg");
+var modalImg = document.getElementById("img");
+
+var captionText = document.getElementById("caption");
+
+function clickNext(id) {
+    modal.style.display = "block";
+    modalImg.src = document.getElementById(id).src;
+    captionText.innerHTML = document.getElementById(id).alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
